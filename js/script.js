673e0c4d6e2ef1cc1cb1c043142da1186f0e3daa
ilyseiris <iimagy@gmail.com>
2015-07-20 22:17:05 -0700

    fix zoom point

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var map = L.map ('map').setView([40.698824, -73.989312], 12);
+var map = L.map ('map').setView([37.766813, -122.41692], 13);
 
 L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',

19d5513f55491a45a47dd75ee23bb5bd609063e3
ilyseiris <iimagy@gmail.com>
2015-07-20 22:07:20 -0700

    fix commas

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var map = L.map ('map').setView([40.698824, -73.989312], 12);
 L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
 	maxZoom: 17,
-	minZoom: 13
+	minZoom: 13,
 }).addTo(map);
 
 var circleIcon = L.icon({
